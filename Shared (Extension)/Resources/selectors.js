c2a811dd2621d0ac9bef6dbc08ac27f27f6b6e62
Tiago Costa <tctiacos@gmail.com>
2025-11-18 09:19:10 +0000

    make google ads colour more red and account for dark mode

--- Shared (Extension)/Resources/selectors.js
+++ Shared (Extension)/Resources/selectors.js
@@ -109,7 +109,7 @@ const cssSelectors = {
 
     // Google
     googleBackgroundCssOn: '#tads, #atvcap .ptJHdc.yY236b.c3mZkd, #tads .CnP9N.U3A9Ac.irmCpc,.commercial-unit-mobile-top,.commercial-unit-mobile-top .v7hl4d,.commercial-unit-mobile-bottom .v7hl4d {background-color: transparent !important;}',
-    googleBackgroundCssOff: '#tads, #atvcap .ptJHdc.yY236b.c3mZkd, #tads .CnP9N.U3A9Ac.irmCpc,.commercial-unit-mobile-top,.commercial-unit-mobile-top .v7hl4d,.commercial-unit-mobile-bottom .v7hl4d {background-color: #F2E6C3 !important; padding: 8px !important; border-radius: 6px !important;}',
+    googleBackgroundCssOff: '#tads, #atvcap .ptJHdc.yY236b.c3mZkd, #tads .CnP9N.U3A9Ac.irmCpc,.commercial-unit-mobile-top,.commercial-unit-mobile-top .v7hl4d,.commercial-unit-mobile-bottom .v7hl4d {padding: 8px !important; border-radius: 6px !important;} @media (prefers-color-scheme: light) { #tads, #atvcap .ptJHdc.yY236b.c3mZkd, #tads .CnP9N.U3A9Ac.irmCpc,.commercial-unit-mobile-top,.commercial-unit-mobile-top .v7hl4d,.commercial-unit-mobile-bottom .v7hl4d {background-color: #F7E0E0 !important;} } @media (prefers-color-scheme: dark) { #tads, #atvcap .ptJHdc.yY236b.c3mZkd, #tads .CnP9N.U3A9Ac.irmCpc,.commercial-unit-mobile-top,.commercial-unit-mobile-top .v7hl4d,.commercial-unit-mobile-bottom .v7hl4d {background-color:rgb(132, 24, 29) !important;} }',
     googleAIOverviewCssOn: '',
     googleAIOverviewCssOff: '#eKIzJc { display: none !important; }',
 
